00deb90bcc7e4275c232692e4feb6f8ec544b0d7
al038968 <al038968@s09-pc17>
2017-05-29 18:23:45 +0200

    proceso

--- public_html/js/cliente/service.js
+++ public_html/js/cliente/service.js
@@ -8,8 +8,8 @@ moduloCliente.factory('clienteService', ['serverService', function (serverServic
                     {name: "nombre", shortname: "Nombre", longname: "Nombre", visible: true, type: "text", required: true, maxlength: 255, pattern: serverService.getRegExpr("nombre"), help: serverService.getRegExpl("nombre")},
                     {name: "primerapellido", shortname: "Apellido 1", longname: "Primer Apellido", visible: true, type: "text", required: true, maxlength: 255, pattern: serverService.getRegExpr("nombre"), help: serverService.getRegExpl("nombre")},
                     {name: "segundoapellido", shortname: "Apellido 2", longname: "Segundo Apellido", visible: true, type: "text", required: true, maxlength: 255, pattern: serverService.getRegExpr("nombre"), help: serverService.getRegExpl("nombre")},
-                    {name: "email", shortname: "Email", longname: "Email", visible: false, type: "text", required: true, maxlength: 255, pattern: serverService.getRegExpr("email"), help: serverService.getRegExpl("email")},
-                    {name: "telefono", shortname: "Teléfono", longname: "Teléfono", visible: false, type: "text", required: false, maxlength: 255, pattern: serverService.getRegExpr("telefono"), help: serverService.getRegExpl("telefono")},
+                    {name: "email", shortname: "Email", longname: "Email", visible: true, type: "text", required: true, maxlength: 255, pattern: serverService.getRegExpr("email"), help: serverService.getRegExpl("email")},
+                    {name: "telefono", shortname: "Teléfono", longname: "Teléfono", visible: true, type: "text", required: false, maxlength: 255, pattern: serverService.getRegExpr("telefono"), help: serverService.getRegExpl("telefono")},
                 ];
             },
             getIcon: function () {
--- public_html/js/lineapedido/service.js
+++ public_html/js/lineapedido/service.js
@@ -4,7 +4,7 @@ moduloLineapedido.factory('lineapedidoService', ['serverService', function (serv
             getFields: function () {
                 return [
                 {name: "id", shortname: "ID", longname: "Identificador", visible: true, type: "id"},
-                {name: "cantidad", shortname: "Cantidad", longname: "Cantidad", visible: true, type: "decimal", required: true},
+                {name: "cantidad", shortname: "Cantidad", longname: "Cantidad", visible: true, type: "integer", required: true},
                 {name: "obj_factura", shortname: "ID Factura", longname: "Factura Id", visible: true, type: "foreign", reference: "factura", descforeign: "id"},
                 {name: "obj_producto", shortname: "Nombre Producto", longname: "ID Producto", visible: true, type: "foreign", reference: "producto", descforeign: "id"},
                 {name: "total", shortname: "total Producto", longname: "Total Producto", visible: true, type: "decimal"}
